4e5566b583d97621f214dfb351271557a384a73e
phucuttis1tg <vananhlele2005@gmail.com>
2025-04-14 15:08:46 +0700

    Add files via upload

---
+++ contexts/AuthContext.js
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState, useMemo } from "react";
+
+import PropTypes from "prop-types";
+import * as miscFuncs from "~/helper/misc-functions";
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        console.log("useEffect AuthProvider");
+        setUser(JSON.parse(miscFuncs.getCookie("user")));
+    }, []);
+
+    return useMemo(
+        () => (
+            <AuthContext.Provider value={{ user, setUser }}>
+                {children}
+            </AuthContext.Provider>
+        ),
+        [children, user],
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
