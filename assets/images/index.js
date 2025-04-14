be4b0a2b61d5d26b5688366561ab437db9e6f6a6
phucuttis1tg <vananhlele2005@gmail.com>
2025-04-14 14:55:30 +0700

    Add files via upload

---
+++ assets/images/index.js
@@ -0,0 +1,9 @@
+const images = {
+    logo: require("./locket-icon.png"),
+    mediaUpload: require("./media-upload.jpg"),
+    spinner: require("./spinner.gif"),
+    eye: require("./eye.png"),
+    noEye: require("./eye-slash.png"),
+};
+
+export default images;
